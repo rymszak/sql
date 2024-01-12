3cb97064208ff9492b74ca5cf4511df7444073d2
rymszak <102522109+rymszak@users.noreply.github.com>
2024-01-12 12:54:43 +0100

    Update 5 stycznia.sql

--- 2024/5 stycznia.sql
+++ 2024/5 stycznia.sql
@@ -39,3 +39,27 @@ select avg(pensja), miasto from pracownicy group by miasto having avg(pensja) > 
 3) select count(tytul) from filmy where rok_produkcji=1984 GROUP by rok_produkcji;
 4) select count(wyporzyczenia.id_kopii) from kopie join wyporzyczenia on kopie.id_filmu=wyporzyczenia.id_filmu where wyporzyczenia.id_filmu=4;
 5) select round(avg(data_zwrotu-data_wypozyczenia),1) as round from wypozyczenia JOIN kopie on kopie.id_kopii=wypozyczenia.id_kopii join filmy on filmy.id_filmu=kopie.id_filmu where filmy.tytul='Ronin';
+
+---------------------------------------------------------------------------------------------------------------
+
+6) SELECT filmy.tytul, round(min(wypozyczenia.data_zwrotu-wypozyczenia.data_wypozyczenia),1) as min,
+  round(max(wypozyczenia.data_zwrotu-wypozyczenia.data_wypozyczenia),1) as max, round(avg(wypozyczenia.data_zwrotu-wypozyczenia.data_wypozyczenia),1) as sre,
+  round(count(kopie.id_kopii),1) as razy FROM `filmy`
+join kopie on kopie.id_filmu=filmy.id_filmu
+join wypozyczenia on wypozyczenia.id_kopii=kopie.id_kopii
+group by filmy.id_filmu;
+
+albo natural join 
+  
+select filmy.tytul, round(min(wypozyczenia.data_zwrotu-wypozyczenia.data_wypozyczenia),1) as min, round(max(wypozyczenia.data_zwrotu-wypozyczenia.data_wypozyczenia),1) as max, 
+  round(avg(wypozyczenia.data_zwrotu-wypozyczenia.data_wypozyczenia),1) as sre, round(count(kopie.id_kopii),1) as razy from filmy
+  NATURAL join kopie natural join wypozyczenia group by filmy.id_filmu;
+
+7)SELECT klienci.imie, klienci.nazwisko, count(wypozyczenia.id_klienta) as "count" FROM `klienci` 
+  join wypozyczenia on wypozyczenia.id_klienta=klienci.id_klienta group by klienci.id_klienta order by count(wypozyczenia.id_klienta) asc;
+
+albo natural 
+SELECT klienci.imie, klienci.nazwisko, count(wypozyczenia.id_klienta) as "count" FROM `klienci` natural join wypozyczenia 
+  group by klienci.id_klienta order by count(wypozyczenia.id_klienta) asc;
+
+8) 
