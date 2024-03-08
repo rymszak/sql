c9a50097665362b585ecff6075a0ae76701a9ed2
rymszak <102522109+rymszak@users.noreply.github.com>
2024-03-08 11:27:32 +0100

    Update widoki_08-03.sql

--- 2024/widoki_08-03.sql
+++ 2024/widoki_08-03.sql
@@ -1,2 +1,44 @@
 
 --widok to zapytanie do bazy danych zapisane pod jakąś nazwą
+
+SELECT GNP as PKB,
+  Name as kraj,
+  continent as kontynent,
+  population as populacja,
+  SurfaceArea as powierzchnia,
+  LifeExpectancy as DlugoscZycia 
+  FROM `country`;
+
+SELECT 
+	GNP as PKB,
+	Name as kraj, 
+	continent as kontynent, 
+	population as populacja, 
+	SurfaceArea as powierzchnia, 
+	LifeExpectancy as DlugoscZycia 
+FROM 
+	`country`
+order by PKB DESC;
+
+CREATE VIEW pozycjaPKB as
+  SELECT 
+	  GNP as PKB,
+	  Name as kraj, 
+	  continent as kontynent, 
+	  population as populacja, 
+	  SurfaceArea as powierzchnia, 
+	  LifeExpectancy as DlugoscZycia 
+  FROM 
+  	`country`
+  order by PKB DESC;
+
+SELECT PKB,kraj FROM `pozycjapkb`
+
+CREATE view miasta AS
+	SELECT 	
+    	Name as nazwa,
+        CountryCode as kodKraju,
+        District as dystrykt,
+        population as populacja
+	from city
+    order by name
