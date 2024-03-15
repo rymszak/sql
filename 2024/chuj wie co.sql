4feaf24fdf1bc8f0c6b92c1c9f9cbedab6fb5430
rymszak <102522109+rymszak@users.noreply.github.com>
2024-03-15 12:20:09 +0100

    Update chuj wie co.sql

--- 2024/chuj wie co.sql
+++ 2024/chuj wie co.sql
@@ -5,3 +5,25 @@ begin
 	SELECT concat(Name, ' ', Continent,' ',Region) from Country where CODE= kod;
 end $$
 DELIMITER ; 
+
+
+DELIMITER $$
+create function witaj()
+returns text
+DETERMINISTIC
+begin 
+	return 'witaj na zajęciach p nierodki SQL';
+END $$
+DELIMITER ;
+select witaj() as 'Powitanie';
+
+
+DELIMITER $$
+CREATE FUNCTION witajUczniu(imie varchar(20))
+returns text
+DETERMINISTIC
+begin 
+return CONCAT('Witaj ' , imie, ' na zajęciach P.Nierodki SQL');
+end $$
+DELIMITER ;
+SELECT witajUczniu('Kamil');
